ae2917f92168c9882825770e7b9e05dd247439b1
Abhishek Agnihotri <89059338+Abhishekagnihotri099@users.noreply.github.com>
2025-01-07 17:42:44 +0530

    Update agent_notes_similarity_search.py

--- agent_notes_similarity_search.py
+++ agent_notes_similarity_search.py
@@ -1,3 +1,4 @@
+import streamlit as st
 from langchain_community.document_loaders import TextLoader
 from langchain_community.vectorstores import FAISS
 from langchain_huggingface import HuggingFaceEmbeddings
